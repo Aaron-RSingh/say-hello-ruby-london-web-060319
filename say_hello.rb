e847ce9a063fb5a2e90e7c753f1a1a778879f700
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-23 16:24:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 def say_hello(name_to_use=nil)
   if name_to_use
-    puts "Hello, #{name_to_use}!"
+    puts "Hello #{name_to_use}!"
   else
     puts "Hello Ruby Programmer!"
   end
