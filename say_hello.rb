488b03a3177a0e155cc25225cecf77c94bc161c3
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-23 16:22:05 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,4 @@
 # Build your say_hello method here
-def say_hello
-  answer = "Kent Beck"
-  puts "Hello #{answer}!"
-end
-
 def say_hello(name_to_use=nil)
   if name_to_use
     "Hello, #{name_to_use}!"
